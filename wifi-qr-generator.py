8a3ab5f4812511231f65329e5b6e336e642cbb77
Martin Nolan <martinnolan_1@live.co.uk>
2024-08-07 21:25:27 +0100

    site update

--- wifi-qr-generator.py
+++ wifi-qr-generator.py
@@ -1,20 +1,27 @@
+from flask import Flask, render_template, request, send_file
 import wifi_qrcode_generator as qr
+from PIL import Image
+import io
+
+app = Flask(__name__, static_url_path='/static')
 
 def generate_wifi_qr(ssid, password, auth_type):
-    """
-    Generate a QR code for WiFi access.
-    :param ssid: SSID of the WiFi network
-    :param password: Password of the WiFi network
-    :param auth_type: Authentication type (WPA, WPA2, WEP, or nopass)
-    """
     qr_code = qr.wifi_qrcode(ssid, hidden=False, authentication_type=auth_type, password=password)
     img = qr_code.make_image(fill='black', back_color='white')
-    file_name = f"wifi_qr_{ssid}.png"
-    img.save(file_name)
-    print(f"WiFi QR code generated and saved as '{file_name}'.")
+    img_io = io.BytesIO()
+    img.save(img_io, 'PNG')
+    img_io.seek(0)
+    return img_io
 
-if __name__ == "__main__":
-    ssid = "***"
-    password = "***"
-    auth_type = "WPA"
-    generate_wifi_qr(ssid, password, auth_type)
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        ssid = request.form['ssid']
+        password = request.form['password']
+        auth_type = request.form['auth_type']
+        qr_code_io = generate_wifi_qr(ssid, password, auth_type)
+        return send_file(qr_code_io, mimetype='image/png')
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
